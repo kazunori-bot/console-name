545658f028de000ccccb0d8bbb2822ef685b6731
FellGill <40915285+FellGill@users.noreply.github.com>
2019-04-17 19:44:39 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+exports = module.exports = function(title) {
+    if (process.platform == 'win32') {
+        process.title = title;
+    } else {
+        process.stdout.write('\x1b]2;' + title + '\x1b\x5c');
+    }
+};
